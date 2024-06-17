1859051622be71c72f72fa589d2f7e47e143e880
Eliel Alves <elielalves.dev@gmail.com>
2024-06-17 00:03:26 -0300

    feat: Add availability route api and blocked dates api

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,4 +12,25 @@ export const globalStyles = globalCss({
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
   },
+
+  '::-webkit-scrollbar': {
+    width: 6,
+  },
+
+  /* Track */
+  '::-webkit-scrollbar-track': {
+    background: '$gray800',
+    borderRadius: 20,
+  },
+
+  /* Handle */
+  '::-webkit-scrollbar-thumb': {
+    background: '$gray500',
+    borderRadius: 12,
+  },
+
+  /* Handle on hover */
+  '::-webkit-scrollbar-thumb:hover': {
+    background: '$gray500',
+  },
 })
